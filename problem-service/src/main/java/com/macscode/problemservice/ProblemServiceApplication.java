cb32148dcf6e167b5215b9703cf05da060c9bc73
Koba Karaputadze <kobakaraputadze701@gmail.com>
2023-08-11 22:41:13 +0400

    Project setup

---
+++ problem-service/src/main/java/com/macscode/problemservice/ProblemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.macscode.problemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemServiceApplication.class, args);
+	}
+
+}
